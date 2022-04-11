d13fefc3868f720855c6718fd95ba4d41189c51c
Matt Ammirati <matt.ammirati@gmail.com>
2022-04-10 18:47:13 -1000

    added a express backend

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Major League Baseball',
+      slug: 'major-baseball',
+      category: 'Balls',
+      image: '/images/p1.jpeg',
+      price: 15,
+      countInStock: 100,
+      brand: 'Rawlings',
+      rating: 4.7,
+      numReviews: 10,
+      description: 'Baseball used in the MLB'
+    },
+    {
+      name: 'NFL Football',
+      slug: 'nfl-football',
+      category: 'Balls',
+      image: '/images/p2.jpeg',
+      price: 55,
+      countInStock: 100,
+      brand: 'Wilson',
+      rating: 4.8,
+      numReviews: 10,
+      description: 'Football used in the NFL'
+    },
+    {
+      name: 'NBA Basketball',
+      slug: 'nba-basketball',
+      category: 'Balls',
+      image: '/images/p3.jpeg',
+      price: 30,
+      countInStock: 100,
+      brand: 'Spalding',
+      rating: 4.4,
+      numReviews: 10,
+      description: 'Basketball used in the NBA'
+    },
+    {
+      name: 'MLS Soccer ball',
+      slug: 'mls-soccer',
+      category: 'Balls',
+      image: '/images/p4.jpeg',
+      price: 35,
+      countInStock: 100,
+      brand: 'Puma',
+      rating: 4.1,
+      numReviews: 10,
+      description: 'Soccerball used in the MLS'
+    }
+  ]
+}
+
+export default data
